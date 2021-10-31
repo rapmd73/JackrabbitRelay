e2be2c863c4f032323a3e85b7ba1f4eb55874a2a
Rose.Heart <rapmd73@gmail.com>
2021-10-31 21:31:45 +0000

    Bug fixes. Code hardening.
    Changes to be committed:
    	modified:   Base/CCXT-PlaceOrder.future
    	modified:   Base/CCXT-PlaceOrder.spot
    	modified:   Base/JackrabbitRelay
    	modified:   Base/Library/JRRconfig.py

--- Base/Library/JRRconfig.py
+++ Base/Library/JRRconfig.py
@@ -9,7 +9,7 @@ import sys
 sys.path.append('/home/JackrabbitRelay/Base/Library')
 from datetime import datetime
 
-Version="0.0.0.0.100"
+Version="0.0.0.0.105"
 LogDirectory="/home/JackrabbitRelay/Logs"
 BaseDirectory='/home/JackrabbitRelay/Base'
 ConfigDirectory='/home/JackrabbitRelay/Config'
