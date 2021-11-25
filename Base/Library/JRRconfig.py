b38ca47dfb88f1dc94f53313273c062a4a1212e4
Rose.Heart <rapmd73@gmail.com>
2021-11-25 17:55:01 +0000

    Bug fix in spot PlaceOrder that would cause the entire asset to be sold, rather than a single position, has been fixed.
    Changes to be committed:
    	modified:   Base/CCXT-PlaceOrder.spot
    	modified:   Base/Library/JRRapi.py
    	modified:   Base/Library/JRRconfig.py
    	new file:   requirements.txt
    	modified:   update

--- Base/Library/JRRconfig.py
+++ Base/Library/JRRconfig.py
@@ -9,7 +9,7 @@ import sys
 sys.path.append('/home/JackrabbitRelay/Base/Library')
 from datetime import datetime
 
-Version="0.0.0.0.250"
+Version="0.0.0.0.300"
 LogDirectory="/home/JackrabbitRelay/Logs"
 BaseDirectory='/home/JackrabbitRelay/Base'
 ConfigDirectory='/home/JackrabbitRelay/Config'
