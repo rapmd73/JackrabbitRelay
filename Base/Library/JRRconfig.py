f6873a218d96382b3c9c1fb12dc5d45fdf1b789b
Rose.Heart <rapmd73@gmail.com>
2021-12-17 21:24:26 +0000

    Decimal percision throughout all code is now 8 digits.
    Changes to be committed:
    	modified:   Base/CCXT-PlaceOrder.future
    	modified:   Base/CCXT-PlaceOrder.spot
    	modified:   Base/Library/JRRapi.py
    	modified:   Base/Library/JRRconfig.py
    	modified:   Extras/Equilibrium/Equilibrium.spot

--- Base/Library/JRRconfig.py
+++ Base/Library/JRRconfig.py
@@ -9,7 +9,7 @@ import sys
 sys.path.append('/home/JackrabbitRelay/Base/Library')
 from datetime import datetime
 
-Version="0.0.0.0.455"
+Version="0.0.0.0.460"
 LogDirectory="/home/JackrabbitRelay/Logs"
 BaseDirectory='/home/JackrabbitRelay/Base'
 ConfigDirectory='/home/JackrabbitRelay/Config'
