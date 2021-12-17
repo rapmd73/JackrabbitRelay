9d295956ce81f270af978403eee4639a1be18694
Rose.Heart <rapmd73@gmail.com>
2021-12-17 00:55:26 +0000

    Bug fix for asset tracking
    Changes to be committed:
    	modified:   Base/Library/JRRconfig.py
    	modified:   Base/Library/JRRsupport.py

--- Base/Library/JRRconfig.py
+++ Base/Library/JRRconfig.py
@@ -9,7 +9,7 @@ import sys
 sys.path.append('/home/JackrabbitRelay/Base/Library')
 from datetime import datetime
 
-Version="0.0.0.0.450"
+Version="0.0.0.0.455"
 LogDirectory="/home/JackrabbitRelay/Logs"
 BaseDirectory='/home/JackrabbitRelay/Base'
 ConfigDirectory='/home/JackrabbitRelay/Config'
