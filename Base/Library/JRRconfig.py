d5dba5758b1b07d3bfb3d238da8a0d8546e2265a
Rose.Heart <rapmd73@gmail.com>
2021-12-18 22:04:33 +0000

    Maximum asset tracking complete
    Changes to be committed:
    	modified:   Base/Library/JRRconfig.py
    	modified:   Base/Library/JRRsupport.py
    	new file:   Base/MaxAsset
    	modified:   install

--- Base/Library/JRRconfig.py
+++ Base/Library/JRRconfig.py
@@ -9,7 +9,7 @@ import sys
 sys.path.append('/home/JackrabbitRelay/Base/Library')
 from datetime import datetime
 
-Version="0.0.0.0.460"
+Version="0.0.0.0.480"
 LogDirectory="/home/JackrabbitRelay/Logs"
 BaseDirectory='/home/JackrabbitRelay/Base'
 ConfigDirectory='/home/JackrabbitRelay/Config'
