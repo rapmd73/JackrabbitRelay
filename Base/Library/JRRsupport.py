497bcf7e347347879bc19caa4074bd62d3ec23b5
Rose.Heart <rapmd73@gmail.com>
2022-01-03 00:13:59 +0000

    **IMPORTANT** MUST READ!
    FTX seems to have changed the way futures are listed.
    
    With Equilibrium, **TRX-PERP** becomes **TRX/USD:USD**
    
    It would appear that FTX is moving towards allowing perpetuals to be in
    more than one quote currrency. This appears to be an exchange level
    change. This could also be CCXT unifying FTX nomenclature to be in line
    with most other exchanges.
    
    There are still some cases where -PERP still works, but i'm not sure for
    how long.
    
    *Remember, when you run the update script, it makes sure **everything** is properly updated.*
    
    Changes to be committed:
    	modified:   Base/Library/JRRapi.py
    	modified:   Base/Library/JRRconfig.py
    	modified:   Base/Library/JRRsupport.py
    	modified:   Extras/Equilibrium/Equilibrium.future
    	modified:   Extras/Equilibrium/Equilibrium.spot

--- Base/Library/JRRsupport.py
+++ Base/Library/JRRsupport.py
@@ -134,7 +134,7 @@ def ReadAssetList(exchange,account,pair,mp,delete):
 
     fw.Unlock()
 
-    if len(coins)>=int(mp) and len(coins)>1:
+    if len(coins)>=int(mp) and not p in coins:
         JRRlog.ErrorLog("MaxAsset Verification",p+' not allowed to trade')
 
     return coins
