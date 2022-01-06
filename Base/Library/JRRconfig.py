8866516e244bbbe256393036d8c9c0aa8b7214ef
Rose.Heart <rapmd73@gmail.com>
2022-01-06 23:48:16 +0000

    Bug fixes. Equilibrium futures now separrates log files for individual coins.
    Changes to be committed:
    	modified:   Base/Library/JRRconfig.py
    	modified:   Extras/Equilibrium/Balance2Lots
    	modified:   Extras/Equilibrium/Equilibrium.future

--- Base/Library/JRRconfig.py
+++ Base/Library/JRRconfig.py
@@ -9,7 +9,7 @@ import sys
 sys.path.append('/home/JackrabbitRelay/Base/Library')
 from datetime import datetime
 
-Version="0.0.0.0.547"
+Version="0.0.0.0.560"
 LogDirectory="/home/JackrabbitRelay/Logs"
 BaseDirectory='/home/JackrabbitRelay/Base'
 ConfigDirectory='/home/JackrabbitRelay/Config'
