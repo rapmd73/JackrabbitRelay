5a605d3f0a0935fdba39464abbb59b37c9bea96a
Rose.Heart <rapmd73@gmail.com>
2022-01-14 03:37:10 +0000

    Equilibrium has been updated to reflect behavioral patterens from the exchange.
    RelayCLI is a commnd line interface designed to test Relay and to place
    manual orders. Its not in the documentation yet and what lead to the
    above update. Will be documented soon.
    
    Changes to be committed:
    	modified:   Base/Library/JRRconfig.py
    	modified:   Extras/Equilibrium/Equilibrium.future
    	new file:   Extras/RelayCLI
    	modified:   update

--- Base/Library/JRRconfig.py
+++ Base/Library/JRRconfig.py
@@ -9,7 +9,7 @@ import sys
 sys.path.append('/home/JackrabbitRelay/Base/Library')
 from datetime import datetime
 
-Version="0.0.0.0.590"
+Version="0.0.0.0.600"
 LogDirectory="/home/JackrabbitRelay/Logs"
 BaseDirectory='/home/JackrabbitRelay/Base'
 ConfigDirectory='/home/JackrabbitRelay/Config'
