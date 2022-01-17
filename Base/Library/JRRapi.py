d3d3f7729dd1abacac5acc31efde5ccfdea8360c
Rose.Heart <rapmd73@gmail.com>
2022-01-17 00:26:46 +0000

    Added BlackCrowHunter.
    WORKING DRAFT and SPOT ONLY.
    
    Changes to be committed:
    	modified:   Base/Library/JRRapi.py
    	modified:   Base/Library/JRRconfig.py
    	new file:   Extras/BlackCrowHunter/Backtest
    	new file:   Extras/BlackCrowHunter/BlackCrowHunter.spot
    	new file:   Extras/BlackCrowHunter/Launcher
    	new file:   Extras/BlackCrowHunter/README.md
    	new file:   Extras/BlackCrowHunter/gpl-3.0.txt
    	new file:   Extras/BlackCrowHunter/install

--- Base/Library/JRRapi.py
+++ Base/Library/JRRapi.py
@@ -197,6 +197,59 @@ def PlaceOrder(exchange, pair, market, action, amount, close, RetryLimit, Reduce
     if retry>=RetryLimit:
         JRRlog.ErrorLog("Placing Order","order unsuccessful")
 
+# Customized fetch OHLCV. Fetches an entire page
+
+def FetchCandles(exchange,pair,tf,CandleCount,RetryLimit):
+    exchangeName=exchange.name.lower()
+    ohlcv=[]
+    retry429=0
+    retry=0
+
+    # For kucoin only, 429000 errors are a mess. Not the best way to
+    # manage them, but the onle way I know of currently to prevent losses.
+
+    # Save the only rate limit and remap it.
+
+    if exchangeName=='kucoin':
+        rleSave=exchange.enableRateLimit
+        rlvSave=exchange.rateLimit
+        exchange.enableRateLimit=True
+        exchange.rateLimit=372
+
+    done=False
+    while not done:
+        try:
+            if CandleCount>0:
+                ohlcv=exchange.fetch_ohlcv(symbol=pair,timeframe=tf,limit=CandleCount)
+            else:
+                ohlcv=exchange.fetch_ohlcv(symbol=pair,timeframe=tf)
+            if ohlcv==[]:
+                ohlcv=None
+        except Exception as e:
+            if exchangeName=='kucoin':
+                x=str(e)
+                if x.find('429000')>-1:
+                    retry429+=1
+            if retry>=RetryLimit:
+                JRRlog.ErrorLog("Fetching OHLCV",e)
+        else:
+            done=True
+
+        if exchangeName=='kucoin':
+            if retry429>=(RetryLimit*7):
+                retry429=0
+                retry+=1
+        else:
+            retry+=1
+
+    if exchangeName=='kucoin':
+        exchange.enableRateLimit=rleSave
+        exchange.rateLimit=rlvSave
+
+    return ohlcv
+
+
+
 # If fetch_ohlcv fails, revert to fetch_ticker and parse it manually
 # if open is None, use low.
 
