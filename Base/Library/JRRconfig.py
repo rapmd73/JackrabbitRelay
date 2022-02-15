19a1cae87d474cf845191502ee37a9cb7deddcf5
Rose Heart <rapmd73@gmail.com>
2022-02-15 21:57:49 +0000

    Exchange that support limit orders only are now supported. Market order type is automatically forced converted to limit on these exchanges.
    Changes to be committed:
    	modified:   Base/Library/JRRapi.py
    	modified:   Base/Library/JRRconfig.py
    	modified:   Extras/AnalyzeAsset
    	modified:   Extras/ListMarkets

--- Base/Library/JRRconfig.py
+++ Base/Library/JRRconfig.py
@@ -9,7 +9,7 @@ import sys
 sys.path.append('/home/JackrabbitRelay/Base/Library')
 from datetime import datetime
 
-Version="0.0.0.0.753"
+Version="0.0.0.0.755"
 LedgerDirectory="/home/JackrabbitRelay/Ledger"
 LogDirectory="/home/JackrabbitRelay/Logs"
 BaseDirectory='/home/JackrabbitRelay/Base'
