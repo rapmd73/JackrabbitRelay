0fe7932899ce41712c558fd2323dfde5017a7184
Rose Heart <rapmd73@gmail.com>
2022-02-25 23:34:42 +0000

    Hot fix in balance data collection process for exchanges that like to take their market list servers offline with no prior notice.
    Changes to be committed:
    	modified:   Base/Library/JRRconfig.py
    	modified:   Extras/Statistics/Balances

--- Base/Library/JRRconfig.py
+++ Base/Library/JRRconfig.py
@@ -9,7 +9,7 @@ import sys
 sys.path.append('/home/JackrabbitRelay/Base/Library')
 from datetime import datetime
 
-Version="0.0.0.0.792"
+Version="0.0.0.0.795"
 BaseDirectory='/home/JackrabbitRelay/Base'
 ConfigDirectory='/home/JackrabbitRelay/Config'
 LedgerDirectory="/home/JackrabbitRelay/Ledger"
