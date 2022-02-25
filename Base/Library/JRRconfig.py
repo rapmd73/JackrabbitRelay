9d9dd21bf1a495beaf06eefe47cf598fbba9d4b1
Rose Heart <rapmd73@gmail.com>
2022-02-25 00:36:48 +0000

    Early stages of charting...
    Code cleanup
    
    Changes to be committed:
    	modified:   Base/Library/JRRconfig.py
    	modified:   Extras/Statistics/Balances
    	renamed:    Extras/Statistics/README.md -> Extras/Statistics/Balances.md
    	new file:   Extras/Statistics/ChartBalances
    	new file:   Extras/Statistics/ChartExchangeBalances
    	modified:   Extras/Statistics/GetExchangeBalances

--- Base/Library/JRRconfig.py
+++ Base/Library/JRRconfig.py
@@ -9,7 +9,7 @@ import sys
 sys.path.append('/home/JackrabbitRelay/Base/Library')
 from datetime import datetime
 
-Version="0.0.0.0.780"
+Version="0.0.0.0.790"
 BaseDirectory='/home/JackrabbitRelay/Base'
 ConfigDirectory='/home/JackrabbitRelay/Config'
 LedgerDirectory="/home/JackrabbitRelay/Ledger"
