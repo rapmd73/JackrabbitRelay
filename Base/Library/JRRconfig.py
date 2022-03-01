e4af06cbd214a26c54257b820f34ac43d4b36690
Rose Heart <rapmd73@gmail.com>
2022-03-01 22:45:08 +0000

    the Statistatorium has passed its first trial runs and is stable. Its minimalistic but functional.
    Changes to be committed:
    	modified:   Base/JackrabbitRelay
    	modified:   Base/Library/JRRconfig.py
    	renamed:    Base/ServeWebPage -> Base/Statistatorium

--- Base/Library/JRRconfig.py
+++ Base/Library/JRRconfig.py
@@ -9,7 +9,7 @@ import sys
 sys.path.append('/home/JackrabbitRelay/Base/Library')
 from datetime import datetime
 
-Version="0.0.0.0.805"
+Version="0.0.0.0.820"
 BaseDirectory='/home/JackrabbitRelay/Base'
 ConfigDirectory='/home/JackrabbitRelay/Config'
 LedgerDirectory="/home/JackrabbitRelay/Ledger"
