93789755f832ea2ce674234c3e61ca499b283b73
Rose Heart <rapmd73@gmail.com>
2022-03-12 17:00:10 +0000

    Equilibrium: marketType bug fixed
    balance bug fixed (hopefully), test carefully
    
    Changes to be committed:
    	modified:   Base/Library/JRRconfig.py
    	modified:   Extras/Equilibrium/Equilibrium.future
    	modified:   Extras/Equilibrium/Equilibrium.spot

--- Base/Library/JRRconfig.py
+++ Base/Library/JRRconfig.py
@@ -9,7 +9,7 @@ import sys
 sys.path.append('/home/JackrabbitRelay/Base/Library')
 from datetime import datetime
 
-Version="0.0.0.0.905"
+Version="0.0.0.0.910"
 BaseDirectory='/home/JackrabbitRelay/Base'
 ConfigDirectory='/home/JackrabbitRelay/Config'
 LedgerDirectory="/home/JackrabbitRelay/Ledger"
