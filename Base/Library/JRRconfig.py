dbff2b0429240a7f5604f7d0eded75e6b0fbe206
Rose Heart <rapmd73@gmail.com>
2022-03-30 15:51:32 +0000

    Hot fix
    Changes to be committed:
    	modified:   Base/Library/JRRapi.py
    	modified:   Base/Library/JRRconfig.py

--- Base/Library/JRRconfig.py
+++ Base/Library/JRRconfig.py
@@ -9,7 +9,7 @@ import sys
 sys.path.append('/home/JackrabbitRelay/Base/Library')
 from datetime import datetime
 
-Version="0.0.0.0.970"
+Version="0.0.0.0.971"
 BaseDirectory='/home/JackrabbitRelay/Base'
 ConfigDirectory='/home/JackrabbitRelay/Config'
 DataDirectory="/home/JackrabbitRelay/Data"
