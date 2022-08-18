ae270fbd48cd662c4c958672567537134a288c41
Rose Heart <rapmd73@gmail.com>
2022-08-18 22:32:18 +0000

    Alpha-1: updated directory structure for version 2
    Changes to be committed:
    	modified:   Base/CCXT-PlaceOrder.future
    	modified:   Base/CCXT-PlaceOrder.spot
    	modified:   Base/JackrabbitRelay
    	modified:   Base/Library/JRRapi.py
    	modified:   Base/Library/JRRccxt.py
    	modified:   Base/Library/JRRconfig.py
    	modified:   Base/Library/JRRledger.py
    	modified:   Base/Library/JRRlog.py
    	modified:   Base/Library/JRRoanda.py
    	modified:   Base/Library/JRRsupport.py
    	modified:   Base/Library/JackrabbitRelay.py
    	modified:   Base/MaxAsset
    	modified:   Base/PlaceOrder.dsr
    	modified:   Base/PlaceOrder.sandbox.spot
    	modified:   Base/PlaceOrder.tester
    	modified:   Base/RelayKill
    	modified:   Base/RelayLauncher
    	modified:   Base/Statistatorium
    	modified:   install

--- Base/Library/JRRconfig.py
+++ Base/Library/JRRconfig.py
@@ -6,18 +6,18 @@
 # All rights reserved unconditionally.
 
 import sys
-sys.path.append('/home/JackrabbitRelay/Base/Library')
+sys.path.append('/home/JackrabbitRelay2/Base/Library')
 from datetime import datetime
 
 Version="0.0.0.0.1120"
-BaseDirectory='/home/JackrabbitRelay/Base'
-ConfigDirectory='/home/JackrabbitRelay/Config'
-DataDirectory="/home/JackrabbitRelay/Data"
-BalancesDirectory='/home/JackrabbitRelay/Statistics/Balances'
-ChartsDirectory='/home/JackrabbitRelay/Statistics/Charts'
-LedgerDirectory="/home/JackrabbitRelay/Ledger"
-LogDirectory="/home/JackrabbitRelay/Logs"
-StatisticsDirectory='/home/JackrabbitRelay/Extras/Statistics'
+BaseDirectory='/home/JackrabbitRelay2/Base'
+ConfigDirectory='/home/JackrabbitRelay2/Config'
+DataDirectory="/home/JackrabbitRelay2/Data"
+BalancesDirectory='/home/JackrabbitRelay2/Statistics/Balances'
+ChartsDirectory='/home/JackrabbitRelay2/Statistics/Charts'
+LedgerDirectory="/home/JackrabbitRelay2/Ledger"
+LogDirectory="/home/JackrabbitRelay2/Logs"
+StatisticsDirectory='/home/JackrabbitRelay2/Extras/Statistics'
 
 NOhtml='<html><title>NO!</title><body style="background-color:#ffff00;display:flex;weight:100vw;height:100vh;align-items:center;justify-content:center"><h1 style="color:#ff0000;font-weight:1000;font-size:10rem">NO!</h1></body></html>'
 
