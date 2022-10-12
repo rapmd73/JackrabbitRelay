6d7437f103c69f5e12c713c9c75cfebfe4404fd1
Rose Heart <rapmd73@gmail.com>
2022-10-12 21:05:33 +0000

    alpha-1: heavy OANDA restructuring and testing.
    Relay framewwork testing and optimizing
    
    Changes to be committed:
    	modified:   Base/Library/JRRoanda.py
    	modified:   Base/Library/JackrabbitRelay.py
    	modified:   Extras/CodeProofs/Balances
    	modified:   Extras/CodeProofs/listOpenOrders
    	modified:   Extras/CodeProofs/readMarkets
    	modified:   install

--- Base/Library/JackrabbitRelay.py
+++ Base/Library/JackrabbitRelay.py
@@ -15,6 +15,7 @@ import requests
 from datetime import datetime
 
 # Framework APIs
+
 import JRRccxt
 import JRRoanda
 
@@ -35,7 +36,7 @@ class JackrabbitLog:
         if filename==None:
             self.logfile=self.basename
         else:
-            self.logfile=self.basename+'.'+filename.replace('/','').replace(':','')
+            self.logfile=self.basename+'.'+filename.replace('/','').replace('-','').replace(':','')
 
     def Write(self,text):
         pid=os.getpid()
@@ -67,12 +68,16 @@ class JackrabbitLog:
         self.Elapsed()
         sys.exit(0)
 
-# The main class for the system. This IS going to be a royal pain in the
-# ass to type, but it also prevent mistakes as the system grows as
-# developes. This will also allow me to build in place as I replace one
-# section at a time.
+# The main class for the system. This IS going to be a royal pain in the ass to
+# type, but it also prevent mistakes as the system grows and developes. This
+# will also allow me to build in place as I replace one section at a time.
 
 # relay=JackrabbitRelay(framework="ccxt",payload=order)
+
+# The config files can also provide the framework and work from the command
+# line.
+
+# relay=JackrabbitRelay()
 
 class JackrabbitRelay:
     def __init__(self,framework=None,payload=None):
@@ -88,49 +93,68 @@ class JackrabbitRelay:
 
         self.NOhtml='<html><title>NO!</title><body style="background-color:#ffff00;display:flex;weight:100vw;height:100vh;align-items:center;justify-content:center"><h1 style="color:#ff0000;font-weight:1000;font-size:10rem">NO!</h1></body></html>'
 
+        # This will be the connector point to any exchange/broker
+        self.Broker=None
+
+        # Initialize The log to just the basename of the program.
         self.JRLog=JackrabbitLog(None)
+
         # the raw payload, or command line
         self.Payload=payload
+
         # Command line arguments
         self.args=None
         self.argslen=0
+
         # This is the main exchange configuration structure, including APIs.
         self.Config=[]
+
         # Convience for uniformity
         self.Exchange=None
         self.Account=None
         self.Asset=None
+
         # API/Secret for a specific account
         self.Keys=[]
+
         # This is the Active API set for rotation and rotation count
         self.Active=[]
-        self.CurrentKey=0
+        self.CurrentKey=-1
+
         # The current order being processed
         self.Order=None
+
         # Result from last operation
         self.Result=None
+
         # this is the framework that defines the low level API, 
         # ie CCXT, OANDA, ROBINHOOD, FTXSTOCKS
         # Framework can be provided by config file
+
+        self.Framework=None
         if framework!=None:
             self.Framework=framework.lower()
-        else:
-            self.Framework=None
-        # Whether or not to rotate keys after every API call
+
+        # Whether or not to rotate keys after every API call. Does NOT apply to
+        # OANDA.
+
         self.ForceRotateKeys=True
 
-        # Process the payload and configuration at initialization of structure
-        # Must be first function called
+        # Process command line. Must be first function called
+
         self.ProcessCommandLine()
 
-        # if no command line, check for stabdard input
+        # if no command line, check for stabdard input, ie. process order
+
         if self.argslen==1:
             self.ProcessPayload()
 
         # Process config file
+
         self.ProcessConfig()
 
         # Login to exchange/Broker
+
         self.Login()
 
     def GetExchange(self):
@@ -189,13 +213,14 @@ class JackrabbitRelay:
             self.JRLog.Error('Processing Payload','Missing action (buy/sell/close) identifier')
         if "Asset" not in self.Order:
             self.JRLog.Error('Processing Payload','Missing asset identifier')
+
         self.Order['Asset']=self.Order['Asset'].upper()
 
         # Set up logging data
 
         if self.Exchange!=None and self.Account!=None and self.Asset!=None:
             lname=f"{self.Exchange}.{self.Account}.{self.Asset}"
-            self.JRLog.SetLogName(lname.replace('/','').replace(':',''))
+            self.JRLog.SetLogName(lname)
 
     # Read the exchange config file and load API/SECRET for a given (sub)account.
     # MAIN is reserved for the main account
@@ -233,6 +258,12 @@ class JackrabbitRelay:
                         self.JRLog.Error("Reading Configuration",'damaged: '+line)
                     if key['Account']==self.Account:
                         # Add identity to account reference
+
+                        # Will need to check for identity before adding global
+                        # identity.
+
+                        # Add the hooks for the logging process.
+
                         if idl!=None:
                             key={ **idl, **key, **logProcess }
                         self.Keys.append(key)
@@ -241,7 +272,8 @@ class JackrabbitRelay:
             if self.Keys==[]:
                 self.JRLog.Error("Reading Configuration",account+' reference not found, check spelling/case')
             else:
-                self.Active=self.Keys[self.CurrentKey]
+                # Initialize to the first key
+                self.Active=self.Keys[0]
         else:
             self.JRLog.Error("Reading Configuration",echg+'.cfg not found in config directory')
 
@@ -267,7 +299,9 @@ class JackrabbitRelay:
         elif self.Exchange!=None:
             lname=f"{self.Exchange}"
 
-        self.JRLog.SetLogName(lname.replace('/',''))
+        self.JRLog.SetLogName(lname)
+
+    # Return command line information to user
 
     def GetArgsLen(self):
         return self.argslen
@@ -281,7 +315,10 @@ class JackrabbitRelay:
     # Rotate API key/Secret
 
     def RotateKeys(self):
-        self.CurrentKey=(os.getpid()%len(self.Keys))
+        if self.CurrentKey<0:
+            self.CurrentKey=(os.getpid()%len(self.Keys))
+        else:
+            self.CurrentKey=((self.CurrentKey+1)%len(self.Keys))
         self.Active=self.Keys[self.CurrentKey]
 
         if self.Framework=='ccxt':
@@ -289,28 +326,37 @@ class JackrabbitRelay:
 
     # Login to a given exchange
 
+    # Active references must be passed as well.
+
     def Login(self):
+        # Sanity check
+
         if self.Framework==None:
             if 'Framework' in self.Active:
                 self.Framework=self.Active['Framework']
             else:
-                self.JRLog.Error("Login",self.Exchange+' framework not given')
+                self.JRLog.Error("Login",self.Exchange+' framework not given in configuration')
+
+        # Market data is loaded automatically. Pull it into the Relay object as
+        # well.
 
         if self.Framework=='ccxt':
             self.ccxt=JRRccxt.ExchangeLogin(self.Exchange,self.Config,self.Active)
+            self.Markets=self.GetMarkets()
         elif self.Framework=='oanda':
-            self.oanda=JRRoanda.ExchangeLogin(self.Exchange,self.Config,self.Active)
+            self.Broker=JRRoanda.oanda(self.Exchange,self.Config,self.Active)
+            self.Markets=self.Broker.Markets
 
     # Get the market list from the exchange
 
     def GetMarkets(self):
         self.RotateKeys()
         if self.Framework=='ccxt':
-            self.Result=JRRccxt.ccxtAPI("load_markets",self.ccxt,self.Active)
+            self.Markets=JRRccxt.ccxtAPI("load_markets",self.ccxt,self.Active)
             return self.Result
         elif self.Framework=='oanda':
-            self.Result=JRRoanda.GetMarkets(self.oanda,self.Active)
-            return self.Result
+            self.Markets=self.Broker.GetMarkets()
+            return self.Markets
 
     # Get the exchange balances. Spot or base market
 
@@ -320,8 +366,10 @@ class JackrabbitRelay:
             self.Result=JRRccxt.ccxtAPI("fetch_balance",self.ccxt,self.Active)
             return self.Result
         elif self.Framework=='oanda':
-            self.Result=JRRoanda.GetBalances(self.oanda,self.Active)
+            self.Result=self.Broker.GetBalances()
             return self.Result
+
+    # Get account balance
 
     def GetBalance(self,asset):
         self.RotateKeys()
@@ -330,7 +378,7 @@ class JackrabbitRelay:
             self.Result=JRRccxt.GetBalance(self.Result,asset)
             return self.Result
         elif self.Framework=='oanda':
-            self.Result=JRRoanda.GetBalance(self.oanda,self.Active)
+            self.Result=self.Broker.GetBalance()
             return self.Result
 
     # Get the exchange positions. For and non-spot market
@@ -341,7 +389,7 @@ class JackrabbitRelay:
             self.Result=JRRccxt.ccxtAPI("fetch_positions",self.ccxt,self.Active)
             return self.Result
         elif self.Framework=='oanda':
-            self.Result=JRRoanda.GetPositions(self.oanda,self.Active)
+            self.Result=self.Broker.GetPositions()
             return self.Result
 
     # Get a single positions. For and non-spot market
@@ -353,7 +401,7 @@ class JackrabbitRelay:
             self.Result=JRRccxt.GetContracts(self.Result,Asset)
             return self.Result
         elif self.Framework=='oanda':
-            self.Result=JRRoanda.GetPosition(self.oanda,self.Active,Asset)
+            self.Result=self.Broker.GetPosition(Asset)
             return self.Result
 
     # Get OHLCV data from exchange
@@ -364,7 +412,7 @@ class JackrabbitRelay:
             self.Result=JRRccxt.ccxtAPI("fetch_ohlcv",self.ccxt,self.Active,**kwargs)
             return self.Result
         elif self.Framework=='oanda':
-            self.Result=JRRoanda.GetOHLCV(self.oanda,self.Active,**kwargs)
+            self.Result=self.Broker.GetOHLCV(**kwargs)
             return self.Result
 
     # Get ticker data from exchange
@@ -375,7 +423,7 @@ class JackrabbitRelay:
             self.Result=JRRccxt.ccxtAPI("fetch_ticker",self.ccxt,self.Active,**kwargs)
             return self.Result
         elif self.Framework=='oanda':
-            self.Result=JRRoanda.GetTicker(self.oanda,self.Active,**kwargs)
+            self.Result=self.Broker.GetTicker(**kwargs)
             return self.Result
 
     # Get orderbook data from exchange
@@ -386,7 +434,7 @@ class JackrabbitRelay:
             self.Result=JRRccxt.GetOrderBook(self.ccxt,self.Active,**kwargs)
             return self.Result
         elif self.Framework=='oanda':
-            self.Result=JRRoanda.GetOrderBook(self.oanda,self.Active,**kwargs)
+            self.Result=self.Broker.GetOrderBook(**kwargs)
             return self.Result
 
     # Get open orders
@@ -397,7 +445,7 @@ class JackrabbitRelay:
             self.Result=JRRccxt.GetOpenOrders(self.ccxt,self.Active,**kwargs)
             return self.Result
         elif self.Framework=='oanda':
-            self.Result=JRRoanda.GetOpenOrders(self.oanda,self.Active,**kwargs)
+            self.Result=self.Broker.GetOpenOrders(**kwargs)
             return self.Result
 
     # Place Order to exchange. Needs to handle buy, sell, close
@@ -417,16 +465,6 @@ class JackrabbitRelay:
             self.Result=JRRccxt.PlaceOrder(self.ccxt,self.Active,**kwargs)
             return self.Result
         elif self.Framework=='oanda':
-            self.Result=JRRoanda.PlaceOrder(self.oanda,self.Active,**kwargs)
+            self.Result=self.Broker.PlaceOrder(**kwargs)
             return self.Result
-
-"""
-
-Needs support for these functions as unified base.
-Exchange response goes in self.Order['Result']
-
-ccxt.fetchOrder         self.Order['Result']=GetOrder
-ccxt.cancel_order       CancelOrder
-ccxt.create_order       CreateOrder
-"""
 
