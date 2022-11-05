42e15bdfddc022c798b04eddef9f2502cc1101aa
Rose Heart <rapmd73@gmail.com>
2022-11-05 22:31:11 +0000

    alpha-1: ledger now works with OliverTwist
    Changes to be committed:
    	modified:   Base/Library/JRRccxt.py
    	modified:   Base/Library/JRRoanda.py
    	modified:   Base/Orphan.ccxt
    	modified:   Base/Orphan.oanda

--- Base/Library/JRRoanda.py
+++ Base/Library/JRRoanda.py
@@ -428,7 +428,8 @@ class oanda:
         ledger['DateTime']=(datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'))
         ledger['ID']=id
         ledger['Order']=Order
-        ledger['Response']=Response
+        if Responswe!=None:
+            ledger['Response']=Response
         ledger['Detail']=detail
 
         if Order['Exchange']!=None and Order['Account']!=None and Order['Asset']!=None:
