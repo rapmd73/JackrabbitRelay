cdf9d95993fe8dc8b4c58aa0d462a004e948bcba
Rose Heart <rapmd73@gmail.com>
2024-10-15 17:15:05 +0000

    Bug with Binance not trading the proper amount is fixed.
    Version update.
    
    Changes to be committed:
    	modified:   Base/CCXT-PlaceOrder.spot
    	modified:   Base/JackrabbitLocker
    	modified:   Base/JackrabbitOliverTwist
    	modified:   Base/JackrabbitRelay
    	modified:   Base/Library/JRRccxt.py
    	modified:   Base/Library/JRRmimic.py
    	modified:   Base/Library/JackrabbitProxy.py
    	modified:   Base/Library/JackrabbitRelay.py
    	modified:   Extras/CodeProofs/findMinCost

--- Base/Library/JRRmimic.py
+++ Base/Library/JRRmimic.py
@@ -45,7 +45,7 @@ class mimic:
     #   placed in init and released at exit.
 
     def __init__(self,Exchange,Config,Active,DataDirectory=None):
-        self.Version="0.0.0.1.995"
+        self.Version="0.0.0.1.1000"
 
         self.StableCoinUSD=['USDT','USDC','BUSD','UST','DAI','FRAX','TUSD', \
                 'USDP','LUSD','USDN','HUSD','FEI','TRIBE','RSR','OUSD','XSGD', \
